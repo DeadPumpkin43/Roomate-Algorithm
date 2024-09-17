2dfc5bff66bd5136ae9d4534b6e02407ae7cb864
DeadPumpkin <masonyeets234@gmail.com>
2024-09-16 21:24:32 -0400

    added other one partly(no loop)
    throws errors from time to time

--- index.js
+++ index.js
@@ -94,3 +94,105 @@ pairArray.forEach((e) => {
   pairNames.push(f);
 });
 console.log(pairNames);
+var addOnArray = [];
+pairArray.forEach((e) => {
+  var arr = [];
+  tempTable["TrueIndex"].values.forEach((a) => {
+    var value = 0;
+    e.forEach((d) => {
+      var personSelfValue = personTable
+        .iloc({ rows: [a], columns: [1, 2, 3, 4] })
+        .values[0].includes(d)
+        ? 0.5
+        : 0;
+
+      var otherValue = personTable
+        .iloc({ rows: [d], columns: [1, 2, 3, 4] })
+        .values[0].includes(a)
+        ? 1
+        : 0;
+
+      value += personSelfValue;
+      value += otherValue;
+      /*console.log(
+        `Self Value:${personSelfValue}\nOther Value:${otherValue}\nIndex:${a}\nOther Index${d}`
+      );*/
+    });
+    console.log(`Index:${a}\nPair:${e}\nValue:${value}`);
+    arr.push(value);
+  });
+  addOnArray.push(arr);
+});
+
+console.log(addOnArray);
+var addon = new dfd.DataFrame(addOnArray, {
+  index: pairArray,
+  columns: tempTable["TrueIndex"].values,
+});
+addon.print();
+
+function checkIfWonOthers(currentIndex, person, minimumNum = null) {
+  var checkDex = addon.index;
+  checkDex.splice(checkDex.indexOf(currentIndex), 1);
+  var highestValue;
+  var foundWin = false;
+  checkDex.forEach((e) => {
+    var locRes = addon.loc({ rows: [e] }).values[0];
+    var locColumn = addon.loc({ rows: [e] }).columns;
+    var winn = maxIndices(locRes);
+    if (winn.length > 1) {
+      return;
+    }
+    if (locColumn[winn[0]] == person) {
+      foundWin = true;
+      highestValue =
+        locRes[winn[0]] > highestValue ? locRes[winn[0]] : highestValue;
+    }
+  });
+
+  if (!minimumNum) {
+    return foundWin;
+  }
+  if (highestValue >= minimumNum && foundWin) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+for (var e in pairArray) {
+  var candidates = addon.loc({ rows: [pairArray[e]] }).values[0];
+  var candidatesTrueIndex = addon.loc({ rows: [pairArray[e]] }).columns;
+  console.log(pairArray[e]);
+  console.log(candidates);
+  var winners = maxIndices(candidates);
+  var realWinner;
+  if (winners.length > 1) {
+    for (var a in winners) {
+      console.log(`Winner:${winners[a]} Winners:${winners}`);
+      if (
+        checkIfWonOthers(
+          e,
+          candidatesTrueIndex[winners[a]],
+          candidates[winners[a]]
+        )
+      ) {
+        realWinner = candidatesTrueIndex[winners[a]];
+        break;
+      }
+    }
+    if (!realWinner) {
+      realWinner =
+        candidatesTrueIndex[winners[random.int(0, winners.length - 1)]];
+    }
+  } else {
+    realWinner = candidatesTrueIndex[winners[0]];
+  }
+  addon.print();
+  console.log(pairArray[e]);
+  addon.drop({ index: [pairArray[e]], inplace: true });
+  addon.drop({ columns: [realWinner], inplace: true });
+  pairArray[e].push(realWinner);
+  console.log(realWinner);
+}
+console.log(pairArray);
